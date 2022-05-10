b2dff0c981cad441a4b14bc3ef5c48bc348daf47
pshemski <p.markowski1980@gmail.com>
2022-05-10 12:10:08 +0100

    run migration for finder and desired ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_09_095223) do
+ActiveRecord::Schema.define(version: 2022_05_09_154152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,19 @@ ActiveRecord::Schema.define(version: 2022_05_09_095223) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "desired_ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "finder_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["finder_id"], name: "index_desired_ingredients_on_finder_id"
+  end
+
+  create_table "finders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -61,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_05_09_095223) do
     t.index ["cuisine_id"], name: "index_recipes_on_cuisine_id"
   end
 
+  add_foreign_key "desired_ingredients", "finders"
   add_foreign_key "recipes", "authors"
   add_foreign_key "recipes", "categories"
   add_foreign_key "recipes", "cuisines"
