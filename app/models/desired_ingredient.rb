16153ff3451235696271f136bb6609eb8ff84eef
pshemski <p.markowski1980@gmail.com>
2022-05-10 12:13:04 +0100

    implement method to collect recipes with desired ingredient

--- app/models/desired_ingredient.rb
+++ app/models/desired_ingredient.rb
@@ -1,3 +1,7 @@
 class DesiredIngredient < ApplicationRecord
   belongs_to :finder
+
+  def recipes
+    Recipe.joins(:ingredients).where(Ingredient.arel_table[:name].matches("%#{name}%"))
+  end
 end
