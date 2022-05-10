2e73abb35ac216251e8bc2946679e3d2a5537405
pshemski <p.markowski1980@gmail.com>
2022-05-10 11:37:39 +0100

    create current pot methods to store new Finder in the session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_pot
+
+  def current_pot
+    if session[:finder_id]
+      Finder.find(session[:finder_id])
+    else
+      Finder.new
+    end
+  end
+
+  def current_pot_or_create
+    cp = current_pot
+    if cp.new_record?
+      cp.save
+      session[:finder_id] = cp.id
+    end
+    cp
+  end
 end
