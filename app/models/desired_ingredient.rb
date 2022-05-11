c221a78a5b0a859c5bfd2dfac2025052bb4067e2
pshemski <p.markowski1980@gmail.com>
2022-05-11 10:07:57 +0100

    add name validation and store recipes into an instance variable

--- app/models/desired_ingredient.rb
+++ app/models/desired_ingredient.rb
@@ -1,7 +1,11 @@
 class DesiredIngredient < ApplicationRecord
+  attr_reader :recipes
+
   belongs_to :finder
 
+  validates :name, presence: true
+
   def recipes
-    Recipe.joins(:ingredients).where(Ingredient.arel_table[:name].matches("%#{name}%"))
+    @recipes ||= Recipe.joins(:ingredients).where(Ingredient.arel_table[:name].matches("%#{name}%"))
   end
 end
